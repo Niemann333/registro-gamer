6af22a4bfe7772f36c1258b34498ae69228d9f0a
Niemann333 <109040743+Niemann333@users.noreply.github.com>
2026-05-06 00:14:35 -0300

    Update sw.js

--- chiptune-player/sw.js
+++ chiptune-player/sw.js
@@ -1,4 +1,4 @@
-// v7 - intercepta soundfonts do gifx.co e redireciona para GitHub
+// v8 - intercepta soundfonts do gifx.co e redireciona para GitHub
 self.addEventListener('install', function(e) { self.skipWaiting(); });
 self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); });
 self.addEventListener('fetch', function(event) {
@@ -12,10 +12,23 @@ self.addEventListener('fetch', function(event) {
     return;
   }
   // Redireciona soundfonts do gifx.co para nosso GitHub
-  if (url.indexOf('gifx.co/soundfonts/') !== -1) {
+  if (url.indexOf('gifx.co/soundfonts/') !== -1 || url.indexOf('/soundfonts/') !== -1) {
     var filename = url.split('/').pop();
     var localUrl = 'https://niemann333.github.io/registro-gamer/chiptune-player/soundfonts/' + filename;
-    event.respondWith(fetch(localUrl));
+    event.respondWith(
+      fetch(localUrl, { mode: 'cors', credentials: 'omit' })
+        .catch(function() {
+          return new Response('', { status: 404 });
+        })
+    );
+    return;
+  }
+  // Bloqueia chiptune.app silenciosamente (falha por CORS)
+  if (url.indexOf('chiptune.app/') !== -1) {
+    event.respondWith(new Response('{}', {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    }));
     return;
   }
 });
