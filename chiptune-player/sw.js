8c25bc0965f49257da8367124daddc90ffaa890d
Niemann333 <109040743+Niemann333@users.noreply.github.com>
2026-05-05 21:14:27 -0300

    Update sw.js

--- chiptune-player/sw.js
+++ chiptune-player/sw.js
@@ -1,12 +1,21 @@
-// v6 - force cache bust
+// v7 - intercepta soundfonts do gifx.co e redireciona para GitHub
 self.addEventListener('install', function(e) { self.skipWaiting(); });
 self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()); });
 self.addEventListener('fetch', function(event) {
   var url = event.request.url;
+  // Redireciona bundle estático
   if (url.indexOf('niemann333.github.io/static/') !== -1) {
     event.respondWith(fetch(url.replace(
       'niemann333.github.io/static/',
       'niemann333.github.io/registro-gamer/chiptune-player/static/'
     )));
+    return;
+  }
+  // Redireciona soundfonts do gifx.co para nosso GitHub
+  if (url.indexOf('gifx.co/soundfonts/') !== -1) {
+    var filename = url.split('/').pop();
+    var localUrl = 'https://niemann333.github.io/registro-gamer/chiptune-player/soundfonts/' + filename;
+    event.respondWith(fetch(localUrl));
+    return;
   }
 });
